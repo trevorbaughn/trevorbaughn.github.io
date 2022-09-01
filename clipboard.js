b7a93b850e4fe83d334764e3ec08b930b17922c2
TheGoatIsBetter <thegoatisbetter@gmail.com>
2022-08-31 17:52:00 -0700

    removed clipboard.js, commented unused links

--- clipboard.js
+++
@@ -1,9 +0,0 @@
-function copyToClipboard(copyText){
-    copyText.select();
-    copyText.setSelectionRange(0,99999); //mobile
-
-    navigator.clipboard.writeText(copyText.value);
-
-    alert("Copied: " + copyText.value);
-
-}
